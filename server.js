01248d63afa728b20af43b2a18bcb59dec2bb857
Mark Swearingen <markswearingen@bendcable.com>
2019-11-12 09:53:09 -0800

    Complete Feature 1. create index.ejs and proof of live with base.css

--- server.js
+++ server.js
@@ -16,6 +16,22 @@ app.use(express.static('public')); //setting server to serve up the static CSS f
 app.set('view engine', 'ejs');
 
 
+//API Route
+app.get('/' ,newSearch);//route for index.ejs, renders the search form
+
+// app.post('/searches', createSearch);
+
+//Helper functions
+//newSearch function to test index.ejs per kanban feature 1
+function newSearch(request, response) {
+  response.render('pages/index');
+};
+
+
 app.get('/', (reg,res) => res.send('Book_App by Mark Swearingen'));
 
-app.listen(PORT, () => console.log('Listening on port ${PORT} '));
+//Catch all
+app.get('*', (request,response) => response.status(404).send('This route does not exist'));
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT} `));
+
