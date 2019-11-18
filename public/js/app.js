ffa2a09946a885a96807ca4557e2e07d21aecb4e
Mark Swearingen <markswearingen@bendcable.com>
2019-11-17 21:30:40 -0800

    removing comments and phantom code

--- public/js/app.js
+++ public/js/app.js
@@ -4,13 +4,6 @@ $(document).ready(function(){
   $('.hide-add-book-form').hide();
 
   $('.show-add-book-form').click(function(){
-//TODO: hide all the other books or the form for them
     $('.hide-add-book-form').show();
-    // $('.book-container').hide();
   });
 });
-
-//Holly Davis provided code below
-// $('.show-add-book-form').on('click', function() {
-//   $(this).next().removeClass('hide-me');
-// });
